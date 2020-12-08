f03642c1b8de738f45bcda26e8d0e6bcf9dc3718
Lukas Ebeling <ga54diz@mytum.de>
2020-12-08 18:28:06 +0100

    Initial Commit

---
+++ CauseCure/CauseCureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CauseCureApp.swift
+//  CauseCure
+//
+//  Created by Lukas Ebeling on 08.12.20.
+//
+
+import SwiftUI
+
+@main
+struct CauseCureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
