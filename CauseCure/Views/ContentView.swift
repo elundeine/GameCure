687bcf15b9cd0a0a1de6aaccb73284fa91226826
Lukas Ebeling <ga54diz@mytum.de>
2021-04-27 11:54:07 +0200

    fixed sign in

--- CauseCure/Views/ContentView.swift
+++ CauseCure/Views/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
             if (session.session != nil) {
                 TabBar()
             } else {
-                SignInView()
+                SignInView(session: self.session)
             }
         }.onAppear(perform: listen)
         
